890b5b726c244539830b9637fcc8d16c583e6947
stevebstorm <steve.lorent@gmail.Com>
2023-07-18 14:11:08 +0200

    Ajout Quizz

---
+++ DemoProject/Pages/Exercices/Exo1/Quizz.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace DemoProject.Pages.Exercices.Exo1
+{
+    public partial class Quizz
+    {
+        public List<string> Responses { get; set; }
+        public bool GameIsFinished { get; set; }
+        public string PlayerName { get; set; }
+        public Quizz()
+        {
+            Responses = new List<string>();
+        }
+
+        public void SaveResponse(string s)
+        {
+            Responses.Add(s);
+        }
+
+        public void GameFinish()
+        {
+            GameIsFinished = true;
+        }
+    }
+}
---
+++ DemoProject/Pages/Exercices/Exo1/QuizzBis.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoProject.Pages.Exercices.Exo1
+{
+    public partial class QuizzBis
+    {
+        [Parameter]
+        public string PlayerName { get; set; }
+        public List<string> QuestionList { get; set; }
+
+        [Parameter]
+        public EventCallback<string> NotifyResponse{ get; set; }
+        [Parameter]
+        public EventCallback NotifyEndGame{ get; set; }
+
+        public string CurrentQuestion { get; set; }
+        public int Counter { get; set; }
+        public QuizzBis()
+        {
+            QuestionList = new List<string>();
+            QuestionList.Add("Aimez vous blazor ?");
+            QuestionList.Add("Avez vous bien manger ce midi ?");
+            QuestionList.Add("Pas trop mal à la tête ?");
+
+            CurrentQuestion = QuestionList[0];
+            Counter = 1;
+        }
+
+        public void SendResponse(string resp)
+        {
+
+            NotifyResponse.InvokeAsync(resp);
+            if(Counter < QuestionList.Count)
+            {
+                CurrentQuestion = QuestionList[Counter++];
+            }
+            else
+            {
+                EndGame();
+            }
+        }
+
+        public void EndGame()
+        {
+            NotifyEndGame.InvokeAsync();
+        }
+    }
+}
