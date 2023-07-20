f5abebdba4b8d62d8be583054d8639e9ad31f4a4
stevebstorm <steve.lorent@gmail.Com>
2023-07-20 10:25:48 +0200

    Ajout Loginpage

---
+++ DemoProject/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoProject.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ DemoProject/Pages/Demo6/Demo6.razor.cs
@@ -0,0 +1,39 @@
+﻿using DemoProject.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DemoProject.Pages.Demo6
+{
+    public partial class Demo6
+    {
+        [Inject]
+        public IJSRuntime _js { get; set; }
+        public User UserLogin { get; set; }
+        private HttpClient _httpClient;
+        public Demo6()
+        {
+            UserLogin = new User();
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7223/api/");
+        }
+
+        public async Task OnSubmit()
+        {
+            string json = JsonConvert.SerializeObject(UserLogin);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+        
+            using(HttpResponseMessage message = await _httpClient.PostAsync("user", content))
+            {
+                if (message.IsSuccessStatusCode)
+                {
+                    string token = await message.Content.ReadAsStringAsync();
+                    await _js.InvokeVoidAsync("localStorage.setItem", "token", token);
+                }
+            }
+        }
+    }
+}
